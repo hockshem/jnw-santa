7555adc4434377bccd44a70944184690a82ad017
Hock Shem <hockshem.c@gmail.com>
2022-12-24 13:25:02 +0800

    Fixed Data Structure

--- member_points.py
+++ member_points.py
@@ -5,25 +5,30 @@ points_df = points_df.fillna("")
 
 def register_new_usr(user_id):
     if user_id not in points_df.index:
-        new_df = pd.Series([0, 0, "", ""], index=points_df.columns).to_frame().T
+        new_df = pd.Series([0, 0], index=points_df.columns).to_frame().T
         new_df.index = [user_id]
         return pd.concat([points_df, new_df])
-
     return points_df
 
 def get_pts_bal(user_id):
-    return points_df.loc[user_id]["Balance"]
+    return points_df.loc[user_id, "Balance"]
 
 def get_pts_acc(user_id):
-    return points_df.loc[user_id]["Accumulated Points"]
+    return points_df.loc[user_id, "Accumulated Points"]
 
 def increase_pts(user_id, amount):
-    points_df.loc[user_id]["Accumulated Points"] += amount
-    points_df.loc[user_id]["Balance"] += amount
+    points_df.loc[user_id, "Accumulated Points"] += amount
+    points_df.loc[user_id, "Balance"] += amount
+    # flush_member_points_data()
     
 def decrease_pts(user_id, amount):
-    points_df.loc[user_id]['Balance'] -= amount
+    points_df.loc[user_id, "Balance"] -= amount
+    # flush_member_points_data()
 
 def flush_member_points_data():
     new_points_df = points_df.reset_index(names="Discord ID")
     new_points_df.to_csv("./csv_data/member_points.csv", index=False)
+
+print(points_df)
+points_df = register_new_usr(9999)
+print(points_df)
