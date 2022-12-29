9b56c8ba79555461d121a0936b096838edaf4b06
Hock Shem <hockshem.c@gmail.com>
2022-12-29 14:18:37 +0800

    Added command to get total spent, fixed the extreme case when image is missing

--- santa.py
+++ santa.py
@@ -11,7 +11,7 @@ from discord.ext import tasks, commands
 from discord.ext import menus
 
 from wish import add_to_prize_pool, update_prize_pool, get_prize_name
-from member_points import increase_pts, get_pts_bal, get_leaderboard
+from member_points import increase_pts, get_pts_bal, get_leaderboard, get_pts_acc
 from leaderboard_menu import LeaderboardPageSource
 from santa_utils import send_event_embed, create_prize_pool_embed
 
@@ -47,7 +47,7 @@ async def give(ctx, pts: int, *members: discord.Member):
         increase_pts(member.id, pts)
         ids.append(f"<@{member.id}>")
     recipients = ', '.join(ids)
-    await ctx.send(f"{recipients} received {pts} points!")
+    await ctx.send(f"{recipients} received `{pts} $JNW`!")
 
 @client.command(name="giveroles")
 async def give(ctx, pts: int, *roles: discord.Role):
@@ -58,12 +58,19 @@ async def give(ctx, pts: int, *roles: discord.Role):
             increase_pts(member.id, pts)
             ids.append(f"<@{member.id}>")
     recipients = ', '.join(ids)
-    await ctx.send(f"{recipients} received {pts} points!")
+    await ctx.send(f"{recipients} received `{pts} $JNW`!")
 
 @client.command()
 async def balance(ctx, member: discord.Member):
     bal = get_pts_bal(member.id)
-    await ctx.send(f"{member.name} has {str(bal)} points!")
+    await ctx.send(f"{member.name} has `{bal} $JNW`!")
+
+@client.command()
+async def spent(ctx, member: discord.Member):
+    acc = get_pts_acc(member.id)
+    bal =  get_pts_bal(member.id)
+    spent = acc - bal
+    await ctx.send(f"{member.name} has spent `{spent} $JNW`!")
 
 @client.command()
 async def prizepool(ctx):
